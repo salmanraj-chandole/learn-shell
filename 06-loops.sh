dd45dbf341ba1de954abb000072d1571af2e45ad
Salman Raj Chandole <salmanrajchandole@gmail.com>
2025-01-24 19:45:29 +0530

    Shell script

---
+++ 06-loops.sh
@@ -0,0 +1,18 @@
+# when we want to execute the commands again and again, then we use loops
+
+# Two loop commands
+# while - if we have expression then we go with this
+# for - if we have inputs then we go with this
+
+i=10
+while [ $i -gt 0 ]; do
+  echo Hello - $i
+  i=$(($i-1))
+  sleep 1
+done
+
+for i in apple banana orange ; do
+    echo Fruit Name - $i
+    sleep 1
+done
+
